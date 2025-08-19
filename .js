7a23edb2bfaea79cc6a4f88d9b23ad7c8735eca3
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-08-19 15:18:57 +0100

    Make grid square and 16x16 instead of 4x4

--- .js
+++ .js
@@ -1,10 +1,10 @@
 let container = document.querySelector(".container")
 
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     let row = document.createElement("div")
     row.className = "row"
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 16; j++) {
         let cell = document.createElement("div")
         cell.className = "cell"
         row.appendChild(cell)

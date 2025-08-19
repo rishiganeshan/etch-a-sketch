e3fe747e176becbcba35b744fe7675536032d072
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-08-19 15:14:42 +0100

    Split grid into rows and change display of all elements to flex

--- .js
+++ .js
@@ -1,8 +1,13 @@
 let container = document.querySelector(".container")
 
 
-for (let i = 0; i < 16; i++) {
-    let cell = document.createElement("div")
-    cell.className = "cell"
-    container.appendChild(cell)
+for (let i = 0; i < 4; i++) {
+    let row = document.createElement("div")
+    row.className = "row"
+    for (let j = 0; j < 4; j++) {
+        let cell = document.createElement("div")
+        cell.className = "cell"
+        row.appendChild(cell)
+    }
+    container.append(row)
 }

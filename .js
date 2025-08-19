4a85a0febf7856945ad606cee25e5cdc7015361d
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-08-19 17:37:47 +0100

    Increment rgb alpha by 10% for each interaction with mouse - looks like a gradual transition to full color

--- .js
+++ .js
@@ -9,6 +9,25 @@ function showPopup() {
     createGrid(numberOfSquares)
 }
 
+function incrementOpacity(cell) {
+    const value = getComputedStyle(cell).getPropertyValue("background-color");
+
+    const parts = value.match(/[\d.]+/g);
+
+    if (parts.length === 3) {
+        parts.push(1);
+    }
+
+    if (parts[3] < 1) {
+        parts[3] = parseFloat(parts[3]) + 0.1;
+    }
+    
+
+    cell.style.backgroundColor = `rgba(${parts.join(',')})`;
+
+
+}
+
 button.addEventListener("click", showPopup)
 
 
@@ -30,7 +49,8 @@ function createGrid(numberOfSquares = 16) {
     let cells = document.querySelectorAll(".cell")
     cells.forEach((cell) => {
         cell.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = "black"
+            // event.target.style.backgroundColor = "black"
+            incrementOpacity(event.target)
         })
 
     })

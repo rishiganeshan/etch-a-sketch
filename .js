fff1c99557c253c0b02931a9ebe1b6e9b80513b3
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-08-19 15:45:12 +0100

    Create button that asks for number of squares on side and draws new grid

--- .js
+++ .js
@@ -1,21 +1,42 @@
 let container = document.querySelector(".container")
+let button = document.querySelector("button")
 
-
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div")
-    row.className = "row"
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement("div")
-        cell.className = "cell"
-        row.appendChild(cell)
+function showPopup() {
+    let numberOfSquares = null
+    while (isNaN(numberOfSquares) || numberOfSquares === null || numberOfSquares > 100 || numberOfSquares <= 0) {
+        numberOfSquares = +prompt('Enter your desired number of squares per side for the new grid (less than 100)')     
     }
-    container.append(row)
+    createGrid(numberOfSquares)
 }
 
-let cells = document.querySelectorAll(".cell")
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "black"
+button.addEventListener("click", showPopup)
+
+
+function createGrid(numberOfSquares = 16) {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    for (let i = 0; i < numberOfSquares; i++) {
+        let row = document.createElement("div")
+        row.className = "row"
+        for (let j = 0; j < numberOfSquares; j++) {
+            let cell = document.createElement("div")
+            cell.className = "cell"
+            row.appendChild(cell)
+        }
+        container.append(row)
+    }
+
+    let cells = document.querySelectorAll(".cell")
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black"
+        })
+
     })
 
-})
+}
+
+createGrid()
+
+
